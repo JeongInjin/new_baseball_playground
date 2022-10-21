950bd729aa7401a5705218050fbce6704b93e3c6
injinjeong <snowwwwww@naver.com>
2022-10-22 08:48:51 +0900

    숫자야구게임 전 commit

--- src/test/kotlin/study/inputStringCalculrator.kt
+++ src/test/kotlin/study/inputStringCalculrator.kt
@@ -6,8 +6,8 @@ import java.util.*
 @DisplayName("입력값을 받아 사칙연산을 수행한다.")
 fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
     println("수식을 입력해 주세요:(ex: 2 + 3 * 4 / 2)")
-    print("=> ")
-    val calculator = Calculator(Scanner(System.`in`).nextLine().toString())
+    print(": ")
+    val calculator = Calculator(readLine().toString())
 
     println("계산 값: ${calculator.result}")
 }
